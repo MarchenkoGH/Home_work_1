b3022d0ffdf53421698a71e2343074f236686e49
Alexander Marchenko <marchenko.alex@gmail.com>
2020-12-07 21:18:26 +0200

    Updated file main.py - add tests for func parse_parameters

--- main.py
+++ main.py
@@ -20,6 +20,19 @@ if __name__ == '__main__':
     assert parse_parameters('http://example.com/?') == {}
     assert parse_parameters('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret',
                                                                                              'color': 'purple'}
+    assert parse_parameters('http://site.ru/page.php?name=dima&age=27') == {'name': 'dima', 'age': '27'}
+    assert parse_parameters('http://httpbin.org/get?key2=value2&key1=value1&key3=value3') == {'key2': 'value2',
+                                                                                              'key1': 'value1',
+                                                                                              'key3': 'value3'}
+    assert parse_parameters('https://www.google.com/search?q=test&oq=t&'
+                            'aqs=chrome.0.69i59j69i57j69i59l2j69i60l4.5093j0j1&sourceid=chrome&ie=UTF-8') \
+           == {'q': 'test', 'oq': 't', 'aqs': 'chrome.0.69i59j69i57j69i59l2j69i60l4.5093j0j1', 'sourceid': 'chrome',
+               'ie': 'UTF-8'}
+
+    assert parse_parameters('https://duckduckgo.com/?q=test&t=h&ia=web') == {'q': 'test', 't': 'h', 'ia': 'web'}
+
+    assert parse_parameters('https://www.google.com/search?client=firefox-b-d&q=k') \
+           == {'client': 'firefox-b-d', 'q': 'k'}
 
     # Tests for function "parse_cookies"
     assert parse_cookies('') == {}
